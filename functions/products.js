64b01886b3ba045257f0d0c8039f87db243257d5
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-03-22 12:29:51 -0700

    update

--- functions/products.js
+++ functions/products.js
@@ -3,51 +3,51 @@ dotenv.config();
 
 const Airtable = require("airtable-node");
 
-const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN })
   .base(process.env.AIRTABLE_BASE)
   .table(process.env.AIRTABLE_TABLE);
 
-  exports.handler = async function () {
-    try {
-      const response = await airtable.list({ maxRecords: 200 });
-  
-      const products = response.records.map((product) => {
-        const { id, fields } = product;
-        const {
-          name,
-          featured,
-          price,
-          colors,
-          company,
-          description,
-          category,
-          shipping,
-          images,
-        } = fields;
-        const { url } = images[0];
-        return {
-          id,
-          name,
-          featured,
-          price,
-          colors,
-          company,
-          description,
-          category,
-          shipping,
-          image: url,
-        };
-      });
-  
+exports.handler = async function () {
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+    console.log(response);
+    const products = response.records ? response.records.map((product) => {
+      const { id, fields } = product;
+      const {
+        name,
+        featured,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        images,
+      } = fields;
+      const { url } = images[0];
       return {
-        statusCode: 200,
-        body: JSON.stringify(products),
+        id,
+        name,
+        featured,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        image: url,
       };
-    } catch (error) {
-      console.log(error);
-      return {
-        statusCode: 500,
-        body: 'There was an error',
-      };
-    }
-  };
+    }) : []; 
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'There was an error',
+    };
+  }
+};
